7b991fbd06eff8468b2369e1f89f28d5849ed278
Rohan Bhujbal <rohan.bhujbal@godiverse.co.uk>
2024-07-29 15:11:43 +0100

    homepage/1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,34 @@
 import "./globals.css";
+import styles from "./home.module.css";
+import Image from "next/image";
 
 export default function Home() {
-  return <main>Hi</main>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thought Agency</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          quos soluta deleniti cumque error, maxime esse beatae? Neque ratione
+          tempora est qui officiis, doloremque maxime aspernatur atque iste a
+          odit.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.buttons}>Learn More</button>
+          <button className={styles.buttons}>Contact Us</button>
+        </div>
+        <div className={styles.brands}>
+          <Image
+            src="/brands.png"
+            alt="brands"
+            fill
+            className={styles.brandImg}
+          />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/hero.gif" alt="hero" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,9 @@ import styles from "./navbar.module.css";
 export default function Navbar() {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Logo</div>
+      <Link href="/" className={styles.logo}>
+        Logo
+      </Link>
       <div>
         <Links />
       </div>
